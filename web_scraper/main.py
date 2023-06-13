b4fde27b65601a8f5ab3aca6819edb46c74abbba
Kasia <kasia.spalinska@gmail.com>
2023-06-13 14:49:38 -0400

    iterating through locations in one city works

--- web_scraper/main.py
+++ web_scraper/main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-from utils import city
+from utils import city, check_exists_by_xpath
 import pandas as pd
 
 URL = "https://www.tripadvisor.com/Search?q=" + city
@@ -19,8 +19,9 @@ print(len(places))
 
 all_titles = []
 all_rtexts = []
+all_places = []
 
-for elem in places:
+for elem, i in zip(places, range(0, len(places))):
     place_name = elem.text.split('\n')[0]
     print(place_name)
 
@@ -29,33 +30,37 @@ for elem in places:
     time.sleep(10)
 
     print(driver.current_url)
-    driver.switch_to.window(driver.window_handles[1])
+    driver.switch_to.window(driver.window_handles[i+1])
     print(driver.current_url)
 
-    for i in range(0, 4):
+    # collecting n pages of reviews
+    for j in range(0, 3):
 
         reviews_section = driver.find_element(By.CSS_SELECTOR, ".eSDnY")
 
+        # collecting review titles
         titles = reviews_section.find_elements(By.CSS_SELECTOR, ".biGQs._P.fiohW.qWPrE.ncFvv.fOtGX")
         for title in titles:
             all_titles.append(title.get_attribute("innerText"))
 
+        # collecting review main texts
         rtexts = reviews_section.find_elements(By.CSS_SELECTOR, ".biGQs._P.pZUbB.KxBGd")
         for rtext in rtexts:
             if rtext.get_attribute("innerText")[0:10] != "Review of:":
                 all_rtexts.append(rtext.get_attribute("innerText"))
 
-        all_rtexts.pop(i*10)
+        all_rtexts.pop(j*10)    # removes unnecessary top of the page element with the same css selector
 
-        next_review_page_button = reviews_section.find_element(By.XPATH, "//a[contains(@aria-label, 'Next page')]")
-        driver.execute_script("arguments[0].click();", next_review_page_button)
-        time.sleep(10)
+        if check_exists_by_xpath(reviews_section, "//a[contains(@aria-label, 'Next page')]"):
+            next_review_page_button = reviews_section.find_element(By.XPATH, "//a[contains(@aria-label, 'Next page')]")
+            driver.execute_script("arguments[0].click();", next_review_page_button)
+            time.sleep(10)
 
-        # driver.switch_to.window(driver.window_handles[i])
-        i += 1
-        print('======== done ========')
+        j += 1
 
-    break
+    i += 1
+    driver.switch_to.window(driver.window_handles[0])
+
 
 print(len(all_titles))
 print(len(all_rtexts))
@@ -63,3 +68,5 @@ print(len(all_rtexts))
 reviews_dict = dict(zip(all_titles, all_rtexts))
 reviews_pd = pd.DataFrame(reviews_dict.items(), columns=['Title', 'Review'])
 reviews_pd.to_csv("trip_advisor_" + city + ".csv")
+
+
--- web_scraper/utils.py
+++ web_scraper/utils.py
@@ -1,3 +1,13 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
 city = 'Barcelona'
 path_to_output_file = 'trip_advisor_'+city+'.csv'
 pages_to_scrape = 1
+
+def check_exists_by_xpath(driver, xpath):
+    try:
+        driver.find_element(By.XPATH, xpath)
+    except NoSuchElementException:
+        return False
+    return True
